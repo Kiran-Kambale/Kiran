c455ae5824b92eceeb70a910523339a94ef08529
Kiran-Kambale <160996406+Kiran-Kambale@users.noreply.github.com>
2024-02-23 13:16:21 +0530

    Update conf.py

--- docs-new/source/conf.py
+++ docs-new/source/conf.py
@@ -11,20 +11,9 @@ copyright = '2024, Kiran'
 author = 'Kiran'
 release = '23.02.2024'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-
-#extensions = ["sphinx.ext.napoleon"]
-#napoleon_google_docstring = False
-
-#extensions = ["readthedocs-sphinx-ext"] 
 
 templates_path = ['_templates']
 exclude_patterns = []
-
-#extensions = ["sphinx.ext.autodoc"] 
-
 
 language = 'English'
 
@@ -33,5 +22,5 @@ language = 'English'
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-#html_logo = '_static/kiransmall.jpg'
+
               
